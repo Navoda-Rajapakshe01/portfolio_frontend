c2e3142627d2091398555c18c6644cfc1d184097
Navoda Rajapakshe <nawoda1112rajapakshe@gmail.com>
2024-05-08 20:29:56 +0530

    first commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#about">About</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#project">Projects</a></li>
+</ul>
+  </div>
+</template>
