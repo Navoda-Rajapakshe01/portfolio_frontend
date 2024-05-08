66e3923139120e142c24c448f5baaa2ae348f480
Navoda Rajapakshe <nawoda1112rajapakshe@gmail.com>
2024-05-08 20:32:41 +0530

    css

--- app.vue
+++ app.vue
@@ -8,3 +8,28 @@
 </ul>
   </div>
 </template>
+<style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: #111;
+}
+</style>
